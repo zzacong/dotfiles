f04dacaa8ba14820ac65ccb981757482087d84e1
Zac Ong <zac.ong@ibm.com>
2023-01-05 15:31:43 +0800

    update upgrade_pnpm_packages.sh from ibmzacong

--- scripts/upgrade_pnpm_packages.sh
+++ scripts/upgrade_pnpm_packages.sh
@@ -3,7 +3,7 @@
 # This script is used to upgrade all pnpm global packages
 #
 if command -v "pnpm" &> /dev/null; then
-	pnpm i -g $(pnpm ls -g | rg '\d$' | sed -e '/^\//d' -e 's/ [0-9]*\.[0-9]*\.[0-9]*//' | xargs)
+	pnpm i -g $(pnpm ls -g | rg '\d$' | sed -e '/^\//d' -e 's/ [0-9]*\.[0-9]*\.[0-9]*//' -e '/^@sitecore/d' | xargs)
 else
 	echo "[ERROR] pnpm is not installed"
 fi
